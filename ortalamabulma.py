ee608b7fa49a2dbb28cc042e6a806012869aa19e
ardacreating <110185492+ardacreating@users.noreply.github.com>
2023-01-24 22:01:12 +0300

    Create ortalamabulma.py

---
+++ ortalamabulma.py
@@ -0,0 +1,6 @@
+sinav1 = int(input("Lütfen birinci sınavın notunu giriniz > "))
+sinav2 = int(input("Lütfen ikinci sınavın notunu giriniz giriniz > "))
+ 
+ortalama = (sinav1 + sinav2) / 2
+ 
+print("Girilen iki sınav notunun ortalaması: ", ortalama)
